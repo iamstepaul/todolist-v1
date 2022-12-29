fa0ffcf570bca08cb72a444f5b3fbc384ab703ba
Stephen Paul <iamstepaul@gmail.com>
2022-12-29 19:28:20 +0100

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js")
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 7000
 
 const items = ["Buy Food", "Cook Food"];
 const workItems = [];
@@ -15,12 +15,12 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
+app.get("/",  (req, res) => {
     let day = date.getDate()
   res.render("list", { listTitle: day, newListItems: items });
 });
-app.post("/", function (req, res) {
-  let item = req.body.newItem;
+app.post("/",  (req, res) => {
+  let item = req.body.newItem
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
@@ -29,18 +29,19 @@ app.post("/", function (req, res) {
     res.redirect("/");
   }
 });
-app.get("/about", function(req, res){
+app.get("/about", (req, res) =>{
     res.render("about")
 })
-app.get("/work", function (req, res) {
+app.get("/work",  (req, res) => {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
-app.post("/work", function (req, res) {
+app.post("/work",  (req, res) => {
   let item = req.body.newItem;
   items.push(item);
   res.redirect("/work");
 });
 
-app.listen(PORT, function () {
-  console.log(`server started at port: ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
 });
+
--- date.js
+++ date.js
@@ -1,4 +1,4 @@
-exports.getDate = function(){
+exports.getDate = () => {
     const today = new Date();
     const options = { weekday: "long", day: "numeric", month: "long" };
     return today.toLocaleDateString("en-US", options);       
